094ae1e1399c15de42146ceab8c3f8b2ab1daf12
Galya Ivanova <GAIvanova20@codingburgas.bg>
2022-05-09 21:26:29 +0300

    Split menu into different functions. Close #6

--- Runtime-error/Runtime-error/menu.cpp
+++ Runtime-error/Runtime-error/menu.cpp
@@ -1,11 +1,7 @@
 #include "menu.h"
 
-void menu()
+void visualiseMenu(string menuButtons[])
 {
-	string menuButtons[3] = {"Play", "Rules", "Quit"};
-	int menuIndex = 0;
-	bool exitGame = 0;
-
 	for (size_t i = 0; i < 3; i++)
 	{
 		if (i == 0)
@@ -17,62 +13,103 @@ void menu()
 			cout << menuButtons[i] << endl;
 		}
 	}
+}
+
+void moveUp(int &menuIndex)
+{
+	if (menuIndex <= 0)
+	{
+		menuIndex = 2;
+	}
+	else
+	{
+		menuIndex--;
+	}
+}
+
+void moveDown(int &menuIndex)
+{
+	if (menuIndex >= 2)
+	{
+		menuIndex = 0;
+	}
+	else
+	{
+		menuIndex++;
+	}
+}
+
+void navigation(int &menuIndex, bool &optionCheck)
+{
+	switch (_getch())
+	{
+	case 'W':
+	case 'w':
+	case 72:
+		moveUp(menuIndex);
+		break;
+	case 'S':
+	case 's':
+	case 80:
+		moveDown(menuIndex);
+		break;
+	case 13:
+		optionCheck = 1;
+		break;
+	}
+}
+void pickedOption(int &menuIndex, bool &exitGame)
+{
+	switch (menuIndex)
+	{
+	case 0:
+		pickGamemode();
+		break;
+	case 1:
+	case 2:
+		exitGame = 1;
+		break;
+	}
+}
+
+void selectedButton(string menuButtons[], int &menuIndex)
+{
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (i == menuIndex)
+		{
+			cout << menuButtons[i] << " <" << endl;
+		}
+		else
+		{
+			cout << menuButtons[i] << endl;
+		}
+	}
+}
+
+void menu()
+{
+	string menuButtons[3] = {"Play", "Rules", "Quit"};
+	int menuIndex = 0;
+	bool exitGame = 0, optionCheck = 0;
+
+	visualiseMenu(menuButtons);
 
 	while (!exitGame)
 	{
 		if (_kbhit())
 		{
-			switch (_getch())
-			{
-				case 'W':
-				case 'w':
-				case 72:
-						if (menuIndex <= 0)
-						{
-							menuIndex = 2;
-						}
-						else
-						{
-							menuIndex--;
-						}
-						break;
-				case 'S':
-				case 's':
-				case 80:
-						if (menuIndex >= 2)
-						{
-							menuIndex = 0;
-						}
-						else
-						{
-							menuIndex++;
-						}
-						break;
-				case 13:
-					switch (menuIndex)
-					{
-					case 0:
-						pickGamemode();
-						break;
-					case 1:
-					case 2:
-						exitGame = 1;
-						break;
-					}
-			}
-
 			system("cls");
 
-			for (size_t i = 0; i < 3; i++)
+			navigation(menuIndex, optionCheck);
+
+			if (!optionCheck)
 			{
-				if (i == menuIndex)
-				{
-					cout << menuButtons[i] << " <" << endl;
-				}
-				else
-				{
-					cout << menuButtons[i] << endl;
-				}
+				selectedButton(menuButtons, menuIndex);
+			}
+			else
+			{	
+				pickedOption(menuIndex, exitGame);
 			}
 		}
 	}
